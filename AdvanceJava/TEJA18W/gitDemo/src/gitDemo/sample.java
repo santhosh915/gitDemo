7d4a02b255bfce5bf9018d37982a5e79a383320b
santhosh <santhoshmca32@gmail.com>
2023-03-06 11:24:35 +0530

    first Commit........

---
+++ AdvanceJava/TEJA18W/gitDemo/src/gitDemo/sample.java
@@ -0,0 +1,20 @@
+package gitDemo;
+
+public class sample {
+	public static void main(String[] args) 
+	{
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("eat something");
+		
+		}
+		else
+		{
+			System.out.println("do work.....");
+		}
+		
+	}
+
+
+}
---
+++ AdvanceJava/TEJA18W/gitDemo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author santhosh
+ *
+ */
+module gitDemo {
+}
